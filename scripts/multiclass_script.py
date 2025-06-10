d41efb95e32f72c76f472653a23452347f0f64fc
JVasconcelos <josevictorvasc@gmail.com>
2025-06-09 23:10:45 -0300

    Added script to download, format and save a simple multiclass dataset

---
+++ scripts/multiclass_script.py
@@ -0,0 +1,24 @@
+import kagglehub
+import shutil
+import os
+import polars as pl
+from sklearn.preprocessing import LabelEncoder
+
+enc = LabelEncoder()
+
+# Download dataset
+path = kagglehub.dataset_download("prathamtripathi/drug-classification")
+# Format downloaded files
+for root, dirs, files in os.walk(path):
+    for file in files:
+        df = pl.read_csv(path+"/"+file)
+        # Label encoding categorical attributes
+        # Saving the old columns to act as reference the labels
+        df = df.with_columns(
+            pl.col("Sex").map_batches(enc.fit_transform).alias("Sex_encoded"),
+            pl.col("BP").map_batches(enc.fit_transform).alias("BP_encoded"),
+            pl.col("Cholesterol").map_batches(enc.fit_transform).alias("Cholesterol_encoded"),
+            pl.col("Drug").map_batches(enc.fit_transform).alias("Drug_encoded"),
+        )
+        # Saving file at the data folder
+        df.write_csv("data/multiclass_drug.csv")
